afc9d7bbc9724dbaf90271e593f8f3a85a43f39e
Makenah Dumont <makenahdumont99@gmail.com>
2023-04-16 13:49:31 -0400

    display snake

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
+let tileCount = 20;
+let tileSize = 18;
+let headX = 10;
+let headY = 10;
 
 function drawGame() {
   let speed = 7;
 
   setTimeout(drawGame, 1000 / speed);
   clearScreen();
+  drawSnake();
 }
 
 function clearScreen() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+}
+
+function drawSnake() {
+  ctx.fillStyle = "orange";
+  ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
 
 drawGame();
