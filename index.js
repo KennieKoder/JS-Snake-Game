8e38507d5d5df3bf37a5a9eddc0a938bf00e098b
Makenah Dumont <makenahdumont99@gmail.com>
2023-04-16 14:06:15 -0400

    enable arrow key control

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let tileCount = 20;
 let tileSize = 18;
 let headX = 10;
 let headY = 10;
+let xvelocity = 0;
+let yvelocity = 0;
+
+document.body.addEventListener("keydown", keyDown);
 
 function drawGame() {
   let speed = 7;
@@ -11,6 +15,7 @@ function drawGame() {
   setTimeout(drawGame, 1000 / speed);
   clearScreen();
   drawSnake();
+  changeSnakePosition();
 }
 
 function clearScreen() {
@@ -21,6 +26,38 @@ function clearScreen() {
 function drawSnake() {
   ctx.fillStyle = "orange";
   ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+}
+
+function keyDown(event) {
+  // up
+  if (event.keyCode == 38) {
+    if (yvelocity == 1) return;
+    yvelocity = -1;
+    xvelocity = 0;
+  }
+  // down
+  if (event.keyCode == 40) {
+    if (yvelocity == -1) return;
+    yvelocity = 1;
+    xvelocity = 0;
+  }
+  // left
+  if (event.keyCode == 37) {
+    if (xvelocity == 1) return;
+    yvelocity = 0;
+    xvelocity = -1;
+  }
+  // right
+  if (event.keyCode == 39) {
+    if (xvelocity == -1) return;
+    yvelocity = 0;
+    xvelocity = 1;
+  }
+}
+
+function changeSnakePosition() {
+  headX = headX + xvelocity;
+  headY = headY + yvelocity;
 }
 
 drawGame();
